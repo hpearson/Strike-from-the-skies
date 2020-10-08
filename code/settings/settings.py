fe9b52f50d9b817f4490080dfeacbdb19d999027
Hunter Pearson <starshiptrooper0589@gmail.com>
2020-10-07 18:44:39 -0600

    Created settings file

---
+++ code/settings/settings.py
@@ -0,0 +1,10 @@
+'''
+Setup a class to hold the game settings information
+'''
+
+
+class Settings():
+    ''' Setup Game Vars '''
+    def __init__(self):
+        ''' Prep Init Values '''
+        self.debug = True
